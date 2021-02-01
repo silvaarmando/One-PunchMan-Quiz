bb4fbd8380d38e38039996cda58a668e0bdecc56
franciscoarmando63 <franciscosilva7877@gmail.com>
2021-02-01 15:22:50 -0300

    Add [id].js

---
+++ pages/quiz/[id].js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function QuizDaGaleraPage() {
+  return (
+    // eslint-disable-next-line react/style-prop-object
+    <div>
+      Desafio da próxima aula junto com as animações.
+    </div>
+  );
+}
